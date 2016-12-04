afcc825a665ad50a864b8bcf0a4f9d1884158660
Ho-Fu Tsai <hofutsai@gmail.com>
2016-12-04 13:14:06 -0800

    Create restaurant entity class

---
+++ src/domain/Restaurant.java
@@ -0,0 +1,46 @@
+package domain;
+
+/**
+ * Created by henry on 12/4/16.
+ */
+public class Restaurant {
+
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Restaurant)) return false;
+
+        Restaurant that = (Restaurant) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        return !(name != null ? !name.equals(that.name) : that.name != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
