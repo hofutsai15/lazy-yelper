d7b88dd11c1ac3dadd16b0e35728eca0e83ac93b
Ho-Fu Tsai <hofutsai@gmail.com>
2016-12-05 00:07:07 -0800

    Change restaurant id field to String type

--- src/domain/Restaurant.java
+++ src/domain/Restaurant.java
@@ -5,15 +5,15 @@ package domain;
  */
 public class Restaurant {
 
-    private Long id;
+    private String id;
 
     private String name;
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
